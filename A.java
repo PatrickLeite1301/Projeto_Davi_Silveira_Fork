c5f35763ec3e701a81eb1be05b2ccd9f059a1a26
Davi Siqueira <davisvsiqueira@gmail.com>
2023-10-01 19:05:13 -0300

    first commit

---
+++ A.java
@@ -0,0 +1,26 @@
+public class A{
+    private int A1;
+    private float A2;
+
+    public int getA1(){
+        return A1;
+    }
+    public float getA2(){
+        return A2;
+    }
+
+    public void setA1(int t){
+        A1 = t;
+    }
+    public void setA2(float t){
+        A2 = t;
+    }
+
+    public void MA1(){
+        System.out.println("MA1");
+    }
+
+    public void MA2(){
+        System.out.println("MA2");
+    }
+}
---
+++ B.java
@@ -0,0 +1,26 @@
+public class B{
+    private int B1;
+    private float B2;
+
+    public int getB1(){
+        return B1;
+    }
+    public float getB2(){
+        return B2;
+    }
+
+    public void setB1(int t){
+        B1 = t;
+    }
+    public void setB2(float t){
+        B2 = t;
+    }
+
+    public void MB1(){
+        System.out.println("MB1");
+    }
+
+    public void MB2(){
+        System.out.println("MB2");
+    }
+}
